170d192ffcbaa9f050dbe5960ace5b036381ce5e
Etolzl <3tolzlgt.21@gmail.com>
2025-10-08 23:29:51 -0600

    IndexedDB y async

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4001;
 
 // Conectar a la base de datos
 conectarDB();
